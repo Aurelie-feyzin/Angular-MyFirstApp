7bb463a126335a4ab64f51376f06d4aa2f4135b1
Aurelie <gramusset.a@gmail.com>
2019-04-19 20:57:12 +0200

    WCS quête 6 : Template driven forms

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {User} from '../model/user';
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+    selector: 'app-sign-up',
+    templateUrl: './sign-up.component.html',
+    styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit {
-  
-  email = 'exemple@wcs.fr';
-  
-  constructor() { }
+export class SignUpComponent {
 
-  ngOnInit() {
-  }
+    public user: User = new User();
+    public submitted = false;
 
+    public constructor() {
+    }
+
+    public onSubmit() {
+        this.submitted = true;
+    }
 }
